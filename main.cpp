4717f3a025a401d1b0f041d7ee75828c545bf243
Miguel <dasilvamigueleduardo@gmail.com>
2024-08-10 21:39:39 -0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+
+
+    string fruta[3] = {"maça","laranja", "uva"};
+    string vegetal[3] = {"abobora", "mandioca", "cara"};
+
+
+    cout << "Hello world!" << endl;
+    return 0;
+}
